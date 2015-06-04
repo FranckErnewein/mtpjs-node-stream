dab05fc24caf079b5291bf8cb93defda4fea3f2b
Franck Ernewein <franck.ernewein@gmail.com>
2015-06-04 12:26:47 +0200

    tranform example

---
+++ example/geekorify.js
@@ -0,0 +1,29 @@
+var stream = require('stream');
+var _ = require('lodash');
+
+var GEEK_LETTERS_MAP = {
+  a: 4,
+  e: 3,
+  i: 1,
+  o: 0,
+  t: 7,
+};
+
+var geekorifier = new stream.Transform();
+
+geekorifier._transform = function(chunk, encoding, callback) {
+
+  var letters = chunk.toString().split('');
+
+  //replace chars with a numbers
+  var transformed = _.map(letters, function(c){
+    return GEEK_LETTERS_MAP[c.toLowerCase()] || c;
+  });
+
+  var result = '-> ' + transformed.join('');
+
+  this.push(result);
+  callback();
+};
+
+process.stdin.pipe(geekorifier).pipe(process.stdout);
